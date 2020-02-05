922773e0f0f9985df833bf7e424683ccc2aaae6a
zino <minzino@hanmail.net>
2020-02-05 21:59:21 +0900

    last commit

--- PalletApp/ViewController.swift
+++ PalletApp/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let colors = [
+        UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue,
+        UIColor.brown ,UIColor.purple
+    ]
+    var arr:Array = Array<UIView>()
+    var index:Int=0
+    
+    @IBOutlet weak var canvas: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        createRect()
     }
-
-
+    
+    func createRect(){
+        for i in 0 ... colors.count-1{
+            var rect = UIView(frame: CGRect(x: 200+(i*52), y: 60, width: 50, height: 50))
+            rect.backgroundColor = colors[i]
+            self.view.addSubview(rect)
+            
+            let gesture = UITapGestureRecognizer(target: self, action: #selector(viewClick))
+            
+            rect.addGestureRecognizer(gesture)
+            arr.append(rect)
+        }
+    }
+    
+    
+    @objc func viewClick(sender:UITapGestureRecognizer){
+        //sender.view?.backgroundColor = UIColor.black
+        canvas.backgroundColor = sender.view?.backgroundColor
+    }
+    
 }
 
